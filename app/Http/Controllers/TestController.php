2e9d2e5c83fc2ab10fdb74f5bd603d86c639bab3
kakedashie1 <alexatleast1@outlook.jp>
2023-06-13 11:54:57 +0900

    destroy

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+use Illuminate\Support\Facades\DB;
+
+class TestController extends Controller
+{
+   public function index()
+   {
+    dd('test');
+    
+    $values = Test::all();
+
+    $count = Test::count();
+
+    $first = Test::findOrFail(1);
+
+    $whereAAA = Test::where('text', '=', 'aaa')->get();
+
+    $queryBuilder = DB::table('tests')->where('text', '=', 'aaa')->select('id','text')->get();
+
+    dd($values,$count,$first,$whereAAA,$queryBuilder);
+
+
+    return view('tests.test',compact('values'));
+   }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +15,35 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('tests/test', [TestController::class, 'index']);
+
+// Route::resource('contacts', ContactFormController::class);
+
+;
+
+Route::prefix('contacts')->middleware(['auth'])->controller(ContactFormController::class)->name('contacts.')->group(function(){
+    Route::get('/', 'index')->name('index');
+    Route::get('/create', 'create')->name('create');
+    Route::post('/', 'store')->name('store');
+    Route::get('/{id}', 'show')->name('show');
+    Route::get('/{id}/edit', 'edit')->name('edit');
+    Route::post('/{id}', 'update')->name('update');
+    Route::post('/{id}/destroy', 'destroy')->name('destroy');
+
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
